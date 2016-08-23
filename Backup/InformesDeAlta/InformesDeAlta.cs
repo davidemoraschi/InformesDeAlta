3fd5ff11affaf1f1af2db2fbe8bad2a68a7ace3d
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 15:00:39 +0200

    moved to GitHub

---
+++ Backup/InformesDeAlta/InformesDeAlta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using System.Windows.Forms;
+
+namespace InformesDeAlta
+{
+    public partial class InformesDeAlta : Form
+    {
+        static string constr = "User Id=DIGAWEB;Password=DIGAWEB;Data Source=DESARROLLO;Enlist=false;Pooling=true";
+        OracleConnection con = new OracleConnection(constr);
+        public InformesDeAlta()
+        {
+            InitializeComponent();
+        }
+
+        private void InformesDeAlta_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            string cmdStr = "SELECT ROWID, IALNHIS, IALNREF, IALINFO FROM VLMFIAL WHERE ROWNUM = 1";
+            OracleCommand cmd = new OracleCommand(cmdStr, con);
+            OracleTransaction txn = con.BeginTransaction();
+            OracleDataReader reader = cmd.ExecuteReader();
+            OracleClob Clob_IALINFO;
+            reader.Read();
+            if (!reader.IsDBNull(0))
+            {
+                //Clob_IALINFO = reader.GetOracleClobForUpdate(3);
+                Clob_IALINFO = reader.GetOracleClob(3);
+                // Close the reader
+                reader.Close();
+                rTxt_Informe.Text = Clob_IALINFO.Value;
+                // Update the job description Clob object
+                //char[] jobDesc = "-SALES".ToCharArray();
+                //Clob_IALINFO.Append(jobDesc, 0, jobDesc.Length);
+                // Now commit the transaction
+                txn.Commit();
+            }
+            else
+                reader.Close();
+        }
+
+        private void InformesDeAlta_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            con.Close();
+            con.Dispose();
+        }
+    }
+}
---
+++ InformesDeAlta/InformesDeAlta.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace InformesDeAlta
+{
+    partial class InformesDeAlta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rTxt_Informe = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rTxt_Informe
+            // 
+            this.rTxt_Informe.Location = new System.Drawing.Point(12, 12);
+            this.rTxt_Informe.Name = "rTxt_Informe";
+            this.rTxt_Informe.Size = new System.Drawing.Size(884, 303);
+            this.rTxt_Informe.TabIndex = 0;
+            this.rTxt_Informe.Text = "";
+            // 
+            // InformesDeAlta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 396);
+            this.Controls.Add(this.rTxt_Informe);
+            this.Name = "InformesDeAlta";
+            this.Text = "Informes de Alta";
+            this.Load += new System.EventHandler(this.InformesDeAlta_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InformesDeAlta_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rTxt_Informe;
+    }
+}
+
